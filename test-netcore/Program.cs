4e000ff9f60c22e7dc61b0f889443263d020bca9
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-09-19 05:43:39 +0200

    add test-netcore template

---
+++ test-netcore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace MyTestApp
+{
+    public class Program
+    {
+        public static string ExecutableName = $"{typeof(Program).Namespace}.exe";
+        public static int Main(string[] args)
+        {
+            if (args.Contains("--debug")) {
+                System.Diagnostics.Debugger.Launch();
+                args = args.Where(a => a != "--debug").ToArray();                
+            }
+            Console.WriteLine("starting Xunit tests");
+            if (args.Count(a => !a.StartsWith("-")) == 0) {
+                var l = args.ToList();
+                
+                l.Add(ExecutableName);
+                args = l.ToArray();
+
+            }
+            using (var program = new  Xunit.Runner.DotNet.Program()) {
+                return program.Run(args);
+            }
+        }
+    }
+}
