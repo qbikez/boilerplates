d9dd994209f7b4a7906fd6cff6655d91f6454488
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-11-22 07:49:42 +0100

    add test-netfx boilerplate

---
+++ test-netfx/test1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Should;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class test1
+    {
+        [Test]
+        public void should_be_true()
+        {
+            var a = true;
+            a.ShouldBeTrue();
+        }
+    }
+}
